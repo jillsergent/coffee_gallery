0e7cbc8a1af809a669b73d60c972279a38abcb6c
Jill <jill.sergent@gmail.com>
2021-12-09 20:30:46 -0500

    added calc js

--- main.js
+++ main.js
@@ -14,3 +14,10 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
